1d00ba874e6f6dc6681b59d96e3115248bcf6536
staticsafe <jogajog@sadiqsaif.com>
2021-09-11 12:44:41 -0400

    ffs nimble be quiet

--- nitter-app/templates/buildnitter.sh
+++ nitter-app/templates/buildnitter.sh
@@ -2,6 +2,6 @@
 # This script runs the build process for nitter and its SCSS
 export PATH=/home/{{ NITTER_USER }}/.nimble/bin:$PATH
 cd /home/{{ NITTER_USER }}/nitter
-nimble -y build -d:release
-nimble -y scss
+nimble -y --silent build -d:release
+nimble -y --silent scss
 mkdir ./tmp
