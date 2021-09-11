53db4971cda3b4c5701bf28aa8db24ef16da7da3
staticsafe <jogajog@sadiqsaif.com>
2021-09-11 12:33:49 -0400

    set PATH in buildnitter.sh because bash is annoying af

--- nitter-app/templates/buildnitter.sh
+++ nitter-app/templates/buildnitter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This script runs the build process for nitter and its SCSS
-
+export PATH=/home/{{ NITTER_USER }}/.nimble/bin:$PATH
 cd /home/{{ NITTER_USER }}/nitter
 nimble -y build -d:release
 nimble -y scss
